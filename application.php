5c924b47eeca470eb8711a347e985291f35d78a1
Makoto <FujiMakoto@users.noreply.github.com>
2015-12-09 15:19:27 -0500

    Content Item Generator command

--- application.php
+++ application.php
@@ -4,6 +4,7 @@
 require __DIR__.'/vendor/autoload.php';
 
 use PowerTools\Console\Proxy;
+use PowerTools\Console\Generate;
 use PowerTools\Console\ClassmapCommand;
 use PowerTools\Console\TinkerCommand;
 use Symfony\Component\Console\Application;
@@ -12,4 +13,5 @@ $application = new Application('Power Tools', '0.1');
 $application->add(new ClassmapCommand());
 $application->add(new TinkerCommand());
 $application->add(new Proxy\RegenCommand());
+$application->add(new Generate\ContentItemCommand());
 $application->run();
